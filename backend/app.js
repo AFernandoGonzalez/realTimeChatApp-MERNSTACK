664d13efd2f7ab3b044917058004838972ce79aa
Fernando <ferg.developer@gmail.com>
2023-12-28 17:01:20 -0500

    added cors origin

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use(cors());
 app.use((req, res, next) => {
     const allowedOrigins = [
         'https://chatappeando.netlify.app',
-        'https://main--chatappeando.netlify.app/'
+        'https://main--chatappeando.netlify.app'
     ];
     const origin = req.headers.origin;
 
