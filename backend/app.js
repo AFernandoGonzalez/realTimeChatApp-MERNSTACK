3e46cf2af879629b79453fbbb89d4ede5a22296a
Fernando <ferg.developer@gmail.com>
2023-12-28 17:19:30 -0500

    updated cors

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
     //     res.setHeader('Access-Control-Allow-Origin', origin);
     // }
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://chatappeando.netlify.app', 'https://main--chatappeando.netlify.app');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Credentials', true);
     next();
